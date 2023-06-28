017c18227b2eda102d4c2c7e1e50f113b5a4f5bd
Andy Cockroft <acockroft1994@gmail.com>
2023-06-28 16:54:25 +0200

    sign up complete

--- routes/auth.routes.js
+++ routes/auth.routes.js
@@ -14,6 +14,21 @@ router.get("/signup", (req, res, next) => {
 router.post("/signup", (req, res, next) => {
   const { username, password } = req.body;
 
+  if (!username || !password) {
+    res.render("auth/signup", {
+      errorMessage: "Please fill out all mandatory fields to sign in",
+    });
+    return;
+  }
+
+  const regex = /(?=.*\d)(?=.*[a-z])(?=.*[A-Z]).{6,}/;
+  if (!regex.test(password)) {
+    res.status(500).render("auth/signup", {
+      errorMessage:
+        "Password needs to have at least 6 chars and must contain at least one number, one lowercase and one uppercase letter.",
+    });
+    return;
+  }
   bcryptjs
     .genSalt(saltRounds)
     .then((salt) => bcryptjs.hash(password, salt))
@@ -26,8 +41,16 @@ router.post("/signup", (req, res, next) => {
     .then((createdUser) => {
       res.redirect("/userProfile");
     })
-    .catch((error) => next(error));
+    .catch((error) => {
+      // copy the following if-else statement
+      if (error instanceof mongoose.Error.ValidationError) {
+        res.status(500).render("auth/signup", { errorMessage: error.message });
+      } else {
+        next(error);
+      }
+    });
 });
+
 //GET USER PROFILE PAGE
 router.get("/userProfile", (req, res, next) => {
   res.render("users/user-profile");
